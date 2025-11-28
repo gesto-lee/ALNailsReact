76de607e84c52a66fbf632b4997b281d6374f5d0
gesto-lee <nandolii.soares@gmail.com>
2025-11-28 13:21:56 -0300

    Update main.jsx.jsx

--- src/main.jsx.jsx
+++ src/main.jsx.jsx
@@ -1,9 +1,26 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 import App from "./App.jsx";
+import AdminLogin from "./AdminLogin.jsx";
+import AdminPanel from "./AdminPanel.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+
+        {/* Página principal do cliente */}
+        <Route path="/" element={<App />} />
+
+        {/* Login da dona */}
+        <Route path="/admin" element={<AdminLogin />} />
+
+        {/* Painel da dona */}
+        <Route path="/painel" element={<AdminPanel />} />
+
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
